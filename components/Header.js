ae25b77f89ed4615803d3a5973fb060e93d0bd36
ivanzad-dotcom <ivan.zad@gmail.com>
2026-03-29 21:06:42 +0200

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: "20px 40px",
+      color: "white"
+    }}>
+      <h1 style={{ fontWeight: 500 }}>Adriavisions</h1>
+    </div>
+  );
+}
