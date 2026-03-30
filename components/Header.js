9ada1d072d65c0029962689c01657a46d9f57258
ivanzad-dotcom <ivan.zad@gmail.com>
2026-03-30 20:29:57 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,3 +4,17 @@ import Link from "next/link";
 <Link href="/gallery">Gallery</Link>
 <Link href="/portfolio">Portfolio</Link>
 <Link href="/admin">Upload</Link>
+<div style={{
+  position: "relative"
+}}>
+  <img ... />
+
+  <div style={{
+    position: "absolute",
+    bottom: 20,
+    left: 20
+  }}>
+    <h1>Adriavisions</h1>
+    <p>Photography archive & portfolio</p>
+  </div>
+</div>
