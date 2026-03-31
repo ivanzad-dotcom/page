1ce2f95c710f4d8c87d14234b17255d0890f4f50
ivanzad-dotcom <ivan.zad@gmail.com>
2026-03-31 17:04:58 +0200

    Create page.js

---
+++ app/admin/page.js
@@ -0,0 +1,10 @@
+import Upload from "../../components/Upload";
+
+export default function Admin() {
+  return (
+    <div>
+      <h1 style={{ padding: 40 }}>Upload</h1>
+      <Upload />
+    </div>
+  );
+}
